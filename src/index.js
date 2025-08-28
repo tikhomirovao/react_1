7fbda6258309ea8daaacad7355c5f397a04e596e
hawri <hawri@DESKTOP-BMM1386>
2025-08-28 15:29:02 +0300

    Try icon

--- src/index.js
+++ src/index.js
@@ -4,8 +4,7 @@ import './index.css';
 import App from './components/App';
 import reportWebVitals from './reportWebVitals';
 
-
-let nav = {"Главная": "/index", "Новости":"/new", "О компании": "/about", "Магазин": "/shop", "Контакты":"/contacts"};
+let nav = {"Главная": "/index", "Новости":"/news", "О компании": "/about", "Магазин": "/shop", "Контакты":"/contacts"};
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
